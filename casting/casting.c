7536f57b38dae0e69a27369db5ea9bfcb66e0cdb
tkdgur1996@gmail.com <tkdgur1996@gmail.com>
2022-04-17 00:28:18 +0900

    형변환

--- casting/casting.c
+++ casting/casting.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
 void main() {
+	//강제 형변환
+	printf("%.2lf\n", (int)8.43 + 2.59);
+	printf("%d\n", (int)(8.43 + 2.59));
+	printf("%d\n", (int)8.43 + (int)2.59);
 
+	//문자 형변환
+	printf("%d\n", 'A' + 1);
+	printf("%c\n", 'A' + 1);
 }
